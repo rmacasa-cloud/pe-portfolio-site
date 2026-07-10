6bc6e057272f5c826de6d4cb1a921752fa86597f
Ramon Macasa <rmacasa@ucsb.edu>
2026-07-10 21:53:26 +0000

    Update redeploy script to restart systemd service instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-tmux kill-server 2>/dev/null
 cd ~/pe-portfolio-site
 git fetch && git reset origin/main --hard
 source .venv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d -s flask "cd ~/pe-portfolio-site && source .venv/bin/activate && flask run --host=0.0.0.0"
+systemctl restart myportfolio
